7267c5cab99f36eb399b68d32f83bd53d42bc9ee
João Pedro Saud Cardoso <joaopedrosaudcardoso@gmail.com>
2024-09-05 16:38:55 -0300

    Refactor post page component and remove unused CSS import

--- src/app/ms/post/page.tsx
+++ src/app/ms/post/page.tsx
@@ -2,47 +2,55 @@
 import Image from "next/image";
 import "./style.css"
 import { useSearchParams } from "next/navigation";
+import { Suspense } from 'react'
 
-export default function Home() {
+function PostOptions() {
   const searchParams = useSearchParams();
   const content = searchParams.get("content");
 
   return (
+    <div className="flex gap-4 items-center flex-col sm:flex-row">
+      <a
+        className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+        href={`https://twitter.com/intent/tweet${content ? `?text=${content}` : ""}`}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Image
+          className="invert"
+          src="/x.svg"
+          alt="X Logo"
+          width={20}
+          height={20}
+        />
+        On X/Twitter
+      </a>
+      <a
+        className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+        href={`https://bsky.app/intent/compose${content ? `?text=${content}` : ""}`}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Image
+          src="/bluesky.svg"
+          alt="Bluesky Logo"
+          width={25}
+          height={25}
+        />
+        On Bluesky
+      </a>
+    </div>
+  );
+}
+
+export default function MsPost() {
+  return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-center">
         <h1 className="text-3xl sm:text-4xl text-center sm:text-center">Where do you like to post this content?</h1>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href={`https://twitter.com/intent/tweet${content ? `?text=${content}` : ""}`}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="invert"
-              src="/x.svg"
-              alt="X Logo"
-              width={20}
-              height={20}
-            />
-            On X/Twitter
-          </a>
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href={`https://bsky.app/intent/compose${content ? `?text=${content}` : ""}`}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/bluesky.svg"
-              alt="Bluesky Logo"
-              width={25}
-              height={25}
-            />
-            On Bluesky
-          </a>
-        </div>
+        <Suspense fallback={<div className="text-sm font-semibold">Loading...</div>}>
+          <PostOptions />
+        </Suspense>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
@@ -51,7 +59,6 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <span className="material-symbols-rounded">headphones</span>
           <div className="hover:underline hover:underline-offset-4">Go to MelodyScout →</div>
         </a>
       </footer>
