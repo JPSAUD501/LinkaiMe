dc61a4eb1efdcab4151ca5e33bf95de703d9e9b7
JPSAUD501 <72423073+JPSAUD501@users.noreply.github.com>
2024-09-08 16:34:56 -0300

    Refactor PostOptions component to improve loading message display

--- src/app/ms/post/page.tsx
+++ src/app/ms/post/page.tsx
@@ -35,7 +35,9 @@ function PostOptions() {
     decompressContent();
   }, [compressedContent, decompressedContent]);
   if (isLoading) {
-    return <div>Loading...</div>;
+    return (
+      <div className="text-sm font-semibold">Decompressing...</div>
+    );
   }
   return (
     <div className="flex gap-4 items-center flex-col sm:flex-row">
