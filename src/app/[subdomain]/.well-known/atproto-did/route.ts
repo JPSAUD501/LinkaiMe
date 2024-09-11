178766e44a97907aca150df77d35da0454284867
João Pedro Saud Cardoso <joaopedrosaudcardoso@gmail.com>
2024-09-11 01:23:16 -0300

    Refactor route.ts to handle subdomain resolution and return appropriate response

--- src/app/[subdomain]/.well-known/atproto-did/route.ts
+++ src/app/[subdomain]/.well-known/atproto-did/route.ts
@@ -1,39 +1,17 @@
 import { NextRequest } from "next/server"
 
-export const maxDuration = 60;
+export const maxDuration = 30;
 export const dynamic = 'force-dynamic';
 
 export async function GET(
   _request: NextRequest,
   { params }: { params: { subdomain: string } }
 ) {
-  const controller = new AbortController();
-  const timeoutId = setTimeout(() => controller.abort(), 10000);
-
-  try {
-    const linkaimeHandlePromise = fetch(`https://public.api.bsky.app/xrpc/com.atproto.identity.resolveHandle?handle=${params.subdomain}`, { signal: controller.signal });
-    const bskyHandlePromise = fetch(`https://public.api.bsky.app/xrpc/com.atproto.identity.resolveHandle?handle=${params.subdomain.replace('.linkai.me', '.bsky.social')}`, { signal: controller.signal });
-    const [linkaimeHandle, bskyHandle] = await Promise.all([linkaimeHandlePromise, bskyHandlePromise]);
-    clearTimeout(timeoutId);
-
-    const linkaimeUserPromise = linkaimeHandle.json();
-    const bskyUserPromise = bskyHandle.json();
-    const [linkaimeUser, bskyUser] = await Promise.all([linkaimeUserPromise, bskyUserPromise]);
-
-    if (linkaimeUser.did !== undefined) {
-      return new Response(linkaimeUser.did);
-    }
-    if (bskyUser.did !== undefined) {
-      if (linkaimeHandle.status !== 400 || linkaimeUser.message !== "Unable to resolve handle") {
-        return new Response('Unable to certify user');
-      }
-      return new Response(bskyUser.did);
-    }
-    return new Response('User not found');
-  } catch (error) {
-    if (error.name === 'AbortError') {
-      return new Response('Bluesky request timed out', { status: 408 });
-    }
-    return new Response('An error occurred', { status: 500 });
+  const bskyHandle = params.subdomain.replace('.linkai.me', '.bsky.social')
+  const bskyUser = await fetch(`https://public.api.bsky.app/xrpc/com.atproto.identity.resolveHandle?handle=${bskyHandle}`)
+  const bskyUserInfo = await bskyUser.json()
+  if (bskyUserInfo.did !== undefined) {
+    return new Response(bskyUserInfo.did)
   }
+  return new Response(`User ${bskyHandle} not found`, { status: 404 })
 }
